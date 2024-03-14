6b4a44aaacd45637353e6b23b5dd246ea0a1073f
younishamrh10 <143172221+younishamrh10@users.noreply.github.com>
2024-03-14 12:36:04 +0100

    kus abok

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+
+class Program
+{
+    static void Main(string [] args)
+    {
+        T Min<T>(T val1, T val2) where T: IComparable<T>
+        {
+
+        }
+
+        T Max<T>(T val1, T val2) where T: IComparable<T>
+        {
+              
+        }
+    }
+
+
+}
